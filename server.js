3964ee06b7a0b169f72ce97bb027c95676401f24
Brendan Neufeld <brendanjneufeld@gmail.com>
2020-05-07 14:07:34 -0400

    added random value to landing page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, look ma, changes!!');
+  res.send(`Hello from http://${HOST}:${PORT} rando: ${Math.random()}`);
 });
 
 app.listen(PORT, HOST);
